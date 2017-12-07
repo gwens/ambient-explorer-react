6c51782bb8409363ff2672c08fe0a202a2e44c29
gwens <gwenanspearing@hotmail.com>
2017-12-07 15:55:12 +0100

    Added form to SearchBar with rubbish date picker.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,7 +3,11 @@ import React from 'react';
 class SearchBar extends React.Component {
   render() {
     return (
-      <p>Search</p>
+      <form>
+        <input type="text" class="search" placeholder="search me..." />
+        <input type="month" />
+        <input type="month" />
+      </form>
     )
   }
 }
