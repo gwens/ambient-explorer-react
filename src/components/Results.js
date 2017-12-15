179f6935d1d71c60ddbcebe16c57543b7f133263
gwens <gwenanspearing@hotmail.com>
2017-12-15 11:02:24 +0100

    Can now change page, but not yet dealing with the cases where there are fewer than maxResults results or you go further than the last result.

--- src/components/Results.js
+++ src/components/Results.js
@@ -4,6 +4,8 @@ import EmailPreview from './EmailPreview';
 class Results extends React.Component {
   render() {
     const searchString = this.props.searchString;
+    const maxResults = 25;
+    const resultsPage = this.props.resultsPage;
     const emails = this.props.emails;
     const emailIds = Object.keys(emails);
     const searchResults = emailIds.reduce((resultsArr, key) => {
@@ -12,12 +14,13 @@ class Results extends React.Component {
       }
       return resultsArr;
     }, []);
+    const pageOfResults = searchResults.slice((resultsPage - 1) * maxResults, resultsPage * maxResults);
     return (
       <div>
         <p>Results Controls</p>
         <ul>
           {
-            searchResults
+            pageOfResults
               .map(key => <EmailPreview key={key} index={key} details={emails[key]} selectEmail={this.props.selectEmail}/>)
           }
         </ul>
